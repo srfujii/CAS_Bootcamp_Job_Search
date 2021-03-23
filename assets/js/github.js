66acf9a92e9cb50eab3386edb55bfc3a1474c959
Cesar Campos <cesarorlandocampos@gmail.com>
2021-03-23 13:26:05 -0500

    adding github.js initial commit and testing of the api

---
+++ assets/js/github.js
@@ -0,0 +1,60 @@
+var searchBtnEl = document.querySelector('#searchBtn'); // Reference to search button
+
+var buttonClickHandler = function(event) {
+    event.preventDefault(); // Prevent default action
+
+    console.log("We clicked the button");
+    getJobData();
+};
+
+function getJobData() {
+    var apiUrl = 'https://jobs.github.com/positions.json?location=remote'
+
+    fetch(apiUrl)
+        .then(function(response) {
+            if (response.ok) {
+                response.json().then(function(data) {
+                    console.log(data);
+                    // If Adzuna returns empty data object, alert the user and go back
+                    if (data == " ") {
+                        alert('Error: ' + 'We could not find that city, please try again.');
+                        return;
+                    }
+                    displayResults(data.results);
+                });
+            } else {
+                alert('Error: ' + response.statusText);
+            }
+        })
+        .catch(function(error) {
+            alert('Unable to connect to GitHub Jobs');
+        });
+}
+
+function displayResults(resultsArray) {
+
+    var searchResultsContainerEL = document.querySelector('#searchResults');
+
+    searchResultsContainerEL.innerHTML = "";
+
+    var searchResultsTitleEl = document.createElement('h3');
+    searchResultsTitleEl.textContent = "Job Search Results: ";
+    searchResultsContainerEL.appendChild(searchResultsTitleEl);
+
+    // Loop through 50 results
+    for (var i = 1; i < resultsArray.length; i++) {
+
+        console.log("Job Type: " + resultsArray[i].type);
+        console.log("Job URL: " + resultsArray[i].url);
+        console.log("Job Created On: " + resultsArray[i].created_at);
+        console.log("Company Name: " + resultsArray[i].company.display_name);
+        console.log("Job Location: " + resultsArray[i].location.display_name);
+        console.log("Job Title: " + resultsArray[i].title);
+        console.log("Description: " + resultsArray[i].description);
+        //console.log("Job Category: " + resultsArray[i].category.label);
+        console.log("Apply: " + resultsArray[i].how_to_apply);
+    }
+};
+
+/*** Event Listeners ***/
+searchBtnEl.addEventListener('click', buttonClickHandler);
