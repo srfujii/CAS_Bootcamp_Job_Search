9a713b821c525b760d16d88bbaedfe5aedbdf17b
Cesar Campos <cesarorlandocampos@gmail.com>
2021-03-24 11:43:41 -0500

    fixed the error now just have to get the display results from the console log onto the page

--- assets/js/github.js
+++ assets/js/github.js
@@ -4,31 +4,46 @@ location:[remote/texas]
 type: fulltime[yes/no{parttime/contract}]
 */
 
-var searchBtnEl = document.querySelector('#searchBtn'); // Reference to search button
+var formEl = document.querySelector('#searchBtn'); // Reference to search button
+var chosenSkill = " ";
 
-var buttonClickHandler = function(event) {
+var FormSubmithandler = function(event) {
     event.preventDefault(); // Prevent default action
     console.log("We clicked the button");
-    getGHJData();
+
+    var pickedValue = document.getElementById("optionSelectBox").value; // Get the selected value number 0-5 from the option that was selected
+    var chosenText = document.getElementById(`${pickedValue}`); // Get reference to html ID associated with selected option
+    chosenText = chosenText.textContent; // Get the Text content from the selected option
+    chosenSkill = chosenText;
+
+    var selectedRemote = document.getElementById("remote");
+
+    if (selectedRemote.checked) {
+        getGithubJobData(chosenSkill);
+    } else {
+        console.log(error)
+        return (error);
+    }
 };
 
-function getGHJData(skillname) {
+function getGithubJobData(skillname) {
+
+    // if (localStorage.getItem(`${skillname}-remote`) !== null) {
+    //     var skillRemoteArray = JSON.parse(localStorage.getItem(`${skillname}-remote`));
+    //     displayRemoteJobResults(remoteResultsArray);
+    // }
+
     var apiUrl = 'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?location=remote&description=' + skillname;
     //var apiUrl = 'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?location=remote&description=javascript'
     /*'https://cors-anywhere.herokuapp.com/' remove this part once deploy to github pages so it doesnt conflict w/ cors*/
 
-    //console.log(apiUrl); //the api url works 
     fetch(apiUrl)
         .then(function(response) {
             if (response.ok) {
                 response.json().then(function(data) {
                     console.log(data);
-                    // If github jobs returns empty data object, alert the user and go back
-                    if (data == "") {
-                        alert('Error: ' + 'We could not find that city, please try again.');
-                        return;
-                    }
-                    displayResults(data.results);
+                    var remoteResultsArray = displayRemoteJobResults(data);
+                    localStorage.setItem(`${skillname}-remote`, JSON.stringify(remoteResultsArray));
                 });
             } else {
                 alert('Error: ' + response.statusText);
@@ -36,32 +51,35 @@ function getGHJData(skillname) {
         })
         .catch(function(error) {
             alert('Unable to connect to GitHub Jobs');
+            console.log(error)
         });
 }
 
-function displayResults(resultsArray) {
+function setRemoteJobs() {}
+
+function displayRemoteJobResults(resultsArray) {
 
     var searchResultsContainerEL = document.querySelector('#searchResults');
-    searchResultsContainerEL.innerHTML = "";
+    searchResultsContainerEL.innerHTML = " ";
 
-    // var searchResultsTitleEl = document.createElement('h3');
-    // searchResultsTitleEl.textContent = "Job Search Results: ";
-    // searchResultsContainerEL.appendChild(searchResultsTitleEl);
-
+    var searchResultsTitleEl = document.createElement('h3');
+    searchResultsTitleEl.textContent = "Job Search Results: ";
+    searchResultsContainerEL.appendChild(searchResultsTitleEl);
+    var skillRemoteArray = [];
     // Loop through 50 results
-    for (var i = 1; i < resultsArray.length; i++) {
+    for (var i = 0; i < resultsArray.length; i++) {
 
         console.log("Job Type: " + resultsArray[i].type);
         console.log("Job URL: " + resultsArray[i].url);
         console.log("Job Created On: " + resultsArray[i].created_at);
-        console.log("Company Name: " + resultsArray[i].company.display_name);
-        console.log("Job Location: " + resultsArray[i].location.display_name);
+        console.log("Company Name: " + resultsArray[i].company);
+        console.log("Job Location: " + resultsArray[i].location);
         console.log("Job Title: " + resultsArray[i].title);
         console.log("Description: " + resultsArray[i].description);
         //console.log("Job Category: " + resultsArray[i].category.label);
         console.log("Apply: " + resultsArray[i].how_to_apply);
     }
+    return (skillRemoteArray);
 };
 
-/*** Event Listeners ***/
-searchBtnEl.addEventListener('click', buttonClickHandler);
+formEl.addEventListener('click', FormSubmithandler);
