e64a8d8e81bfe9618ce95c2a4fd9259e11fbda99
Susan Fujii <susan.fujii@me.com>
2021-03-23 23:24:36 -0500

    Created jobObject and skillLocationArray to save searches to local storage for faster repeat searches. Created new setTexasJobResults function and renamed get functions to getTexasJobData and display function to displayTexasJobResults so that Cesar can add his functions for his remote queries. Now if local storage search exists, we pull from that rather than issuing new fetch request.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,7 @@
 /*** Global Variables  ***/
 var formEl = document.querySelector('#searchForm');     // Get reference to our Search Form
 var selectedSkill = "";                                 // Global variable for selected skill
-var jobObject = {                                       // Job Object populated with job info
-        jobTitle: "",                                       
-        jobCategory: "",
-        companyName: "",
-        creationDate: "",
-        jobLocation: "",
-        jobDesc: "",
-        jobURL: ""
-    };
-var skillLocationArray = [];                            // An array of job Objects    
+
 
 /*** Function formSubmitHandler: called when user clicks "Search" button on form ***/
 function formSubmitHandler (event) {
@@ -44,8 +35,8 @@ function getTexasJobData (skillName) {
 
      // Check to see if skillset-location is already stored in local storage, if so, retrieve & display
      if (localStorage.getItem(`${skillName}-texas`) !== null) {
-       // skillLocationArray = JSON.parse(localStorage.getItem(`${skillName}-texas`));
-       // displayTexasResults();
+       var skillLocationArray = JSON.parse(localStorage.getItem(`${skillName}-texas`));
+       displayTexasJobResults(skillLocationArray);
     } else {
         // Set our fetch URL to skill selected
         var apiUrl = 'https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=81c88f02&app_key=8fd8923d7be696f1f642efb26fcc6fd7&results_per_page=50&what=' + skillName + '&where=Texas';
@@ -54,7 +45,9 @@ function getTexasJobData (skillName) {
                 .then(function (response) {
                 if (response.ok) {
                     response.json().then(function (data) {
-                        displayTexasResults(data.results);
+                        var searchResultArray = setTexasJobResults(data.results);
+                        displayTexasJobResults(searchResultArray);
+                        localStorage.setItem(`${skillName}-texas`, JSON.stringify(searchResultArray));
                     });
                 } else {
                     alert('Error: ' + response.statusText);
@@ -66,24 +59,22 @@ function getTexasJobData (skillName) {
     }
 }
 
-// function setTexasJobResults () {
 
-//     for (var i = 0; i < resultsArray.length; i++) {
-//         jobObject.jobTitle = 
-//     }
+function setTexasJobResults (resultsArray) {
 
-// }
+    var skillLocationArray = [];                            // An array of job Objects
 
-/*** Function displayTexasResults: called by getTexasData to display the results of our fetch.
- *      Loops through 50 results and dynamically displays them to the HTML. ***/
-function displayTexasResults (resultsArray) {
+    for (var i = 0; i < resultsArray.length; i++) {
 
-    // Empty old results before displaying new ones
-    var searchResultsContainerEL = document.querySelector('#searchResults');
-    searchResultsContainerEL.innerHTML = "";
-
-    // Loop through 50 results
-    for (var i = 1; i < resultsArray.length; i++) {
+        var jobObject = {                                   // Job Object populated with job info
+            jobTitle: "",                                       
+            jobCategory: "",
+            companyName: "",
+            creationDate: "",
+            jobLocation: "",
+            jobDesc: "",
+            jobURL: ""
+        };
 
         // Sanitize our results data, remove <strong> and </strong> tags
         var jobTitleText = resultsArray[i].title.replace(/<strong>/g, '');
@@ -94,6 +85,32 @@ function displayTexasResults (resultsArray) {
         // Sanitize URL data, remove ""
         var jobURLText = resultsArray[i].redirect_url.replace(/\"/g, '');
 
+        // Set Job Object data
+        jobObject.jobTitle = jobTitleText;
+        jobObject.jobCategory = resultsArray[i].category.label;
+        jobObject.companyName = resultsArray[i].company.display_name;
+        jobObject.creationDate = resultsArray[i].created;
+        jobObject.jobLocation = resultsArray[i].location.display_name;
+        jobObject.jobDesc = jobDescriptionText;
+        jobObject.jobURL = jobURLText;
+
+        // Add new job object to end of our array
+        skillLocationArray.push(jobObject);
+    }
+    return(skillLocationArray);
+}
+
+/*** Function displayTexasResults: called by getTexasData to display the results of our fetch.
+ *      Loops through 50 results and dynamically displays them to the HTML. ***/
+function displayTexasJobResults (resultsArray) {
+
+    // Empty old results before displaying new ones
+    var searchResultsContainerEL = document.querySelector('#searchResults');
+    searchResultsContainerEL.innerHTML = "";
+
+    // Loop through 50 results
+    for (var i = 0; i < resultsArray.length; i++) {
+
         //Div to contain individual job result info
         var outerDivEl = document.createElement('div');
         outerDivEl.classList = 'card blue-grey darken-1';
@@ -102,29 +119,30 @@ function displayTexasResults (resultsArray) {
         innerDivEl.classList = 'card-content white-text';
 
         var spanJobTitleEl = document.createElement('span');
-        spanJobTitleEl.textContent = jobTitleText;
+        spanJobTitleEl.textContent = resultsArray[i].jobTitle;
 
         var pJobCategoryEl = document.createElement('p');
-        pJobCategoryEl.textContent = resultsArray[i].category.label;
+        pJobCategoryEl.textContent = resultsArray[i].jobCategory;
 
         var pCompanyNameEl = document.createElement('p');
-        pCompanyNameEl.textContent = resultsArray[i].company.display_name;
+        pCompanyNameEl.textContent = resultsArray[i].companyName;
 
         var pJobCreationDateEl = document.createElement('p');
-        pJobCreationDateEl.textContent = resultsArray[i].created;
+        pJobCreationDateEl.textContent = resultsArray[i].creationDate;
 
         var pJobLocationEl = document.createElement('p');
-        pJobLocationEl.textContent = resultsArray[i].location.display_name;
+        pJobLocationEl.textContent = resultsArray[i].jobLocation;
 
         var pJobDescriptionEl = document.createElement('p');
-        pJobDescriptionEl.textContent = jobDescriptionText;
+        pJobDescriptionEl.textContent = resultsArray[i].jobDesc;
 
         var anchorDiv = document.createElement('div');
         anchorDiv.classList = 'card-action';
 
         var aJobURLEl = document.createElement('a');
-        aJobURLEl.setAttribute('href', `${resultsArray[i].redirect_url}`);
-        aJobURLEl.textContent = "Click here for more details and to apply for " + jobTitleText;
+        aJobURLEl.setAttribute('href', resultsArray[i].jobURL);
+        // aJobURLEl.setAttribute('href', `${resultsArray[i].redirect_url}`);
+        aJobURLEl.textContent = "Click here for more details and to apply for " + resultsArray[i].jobTitle;
         
         searchResultsContainerEL.appendChild(outerDivEl);
         outerDivEl.appendChild(innerDivEl);
